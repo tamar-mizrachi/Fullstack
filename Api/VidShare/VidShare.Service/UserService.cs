e826089600e6f3352327d6f905e8cdb8ec062335
Tamar Mizrachi <t0583279405@gmail.com>
2025-11-17 00:03:14 +0200

    update register

--- Api/VidShare/VidShare.Service/UserService.cs
+++ Api/VidShare/VidShare.Service/UserService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -52,6 +52,68 @@ namespace VidShare.Service
         {
 
             return  await _irepositoryManager.Users.GetByLoginAsync(name,password);
+        }
+    }
+}
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidShare.Core.Models;
+using VidShare.Core.Repositories;
+using VidShare.Core.Services;
+
+namespace VidShare.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepositoryManager _irepositoryManager;
+
+        public UserService(IRepositoryManager irepositoryManager)
+        {
+            _irepositoryManager = irepositoryManager;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _irepositoryManager.Users.GetAllAsync();
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await _irepositoryManager.Users.GetByIdAsync(id);
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            var newUser = await _irepositoryManager.Users.AddAsync(user);
+            await _irepositoryManager.SaveAsync();
+            return newUser;
+        }
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            var newUser = await _irepositoryManager.Users.UpdateAsync(user);
+            await _irepositoryManager.SaveAsync();
+            return newUser;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var user = await _irepositoryManager.Users.GetByIdAsync(id);
+            await _irepositoryManager.Users.DeleteAsync(user);
+            await _irepositoryManager.SaveAsync();
+        }
+
+        // ✔️ משאירים את השם כמו שהוא – אבל מסנכרנים אימות
+        public async Task<User?> GetByLoginAsync(string name, string email)
+        {
+            // מכיוון שב-Repository קיימת הפונקציה עם password,
+            // נחשיב email בתור password כדי לשמור תאימות
+            return await _irepositoryManager.Users.GetByLoginAsync(name, email);
         }
     }
 }
